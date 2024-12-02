a482e21bc077bdd4dbb99ba3fd8747d0cbee765b
Patrick Simmons <hadroncollides@gmail.com>
2024-12-02 06:13:05 -0800

    beginning css/constants/statevariables

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
  /*----- constants -----*/
- const MAX_GUESSES = 6
- const SONG_LYRICS = []
+ const maxGuesses = 6;
+const songLyrics = []
 
   /*----- state variables -----*/
-
-
+ let currentWord = ''
+ let guessedLetters = []
+ let wrongGuesses = 0;
+ let gameStarted = false;
   /*----- cached elements  -----*/
-
+  const letterBoxes = document.querySelectorAll('.letter-box');
+  const guessButton = document.getElementById('guessButton');
+  const gameImage = document.querySelector('.game-image');
 
   /*----- event listeners -----*/
 
 
   /*----- functions -----*/
+  function init()
