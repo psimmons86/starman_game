f1d3dae79742ba8358aa5f393d32d2dd14c779a1
Patrick Simmons <hadroncollides@gmail.com>
2024-12-02 08:35:40 -0800

    init pt.1, cached elements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
  /*----- constants -----*/
- const maxGuesses = 6;
+const maxGuesses = 6
 const songLyrics = []
 
   /*----- state variables -----*/
  let currentWord = ''
  let guessedLetters = []
- let wrongGuesses = 0;
- let gameStarted = false;
+ let wrongGuesses = 0
+ let gameStarted = false
   /*----- cached elements  -----*/
-  const letterBoxes = document.querySelectorAll('.letter-box');
-  const guessButton = document.getElementById('guessButton');
-  const gameImage = document.querySelector('.game-image');
+ const letterBoxes = document.querySelectorAll('.letter-box')
+ const guessButton = document.getElementById('guessButton')
+ const gameImage = document.querySelector('.game-image')
+ const songInfoDisplay = document.querySelector('.song-info')
+ const hintDisplay = document.querySelector('.hint-display')
+ const wordDisplay = document.querySelector('.word-display')
 
   /*----- event listeners -----*/
 
+guessButton.addEventListener('click', init)
+
 
   /*----- functions -----*/
-  function init()
+  function init() {
+    const randomSong = songLyrics[Math.floor(Math.random() * songLyrics.length)]
+    currentWord = randomSong.lyric
+
+    guessedLetters = new Set()
+    wrongGuesses = 0
+    gameStarted = true
+
+    guessButton.textContent = 'Playing...'
+  }
+
